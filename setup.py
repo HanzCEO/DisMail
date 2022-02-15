180a42d45d6154e3e2b76a7ef01284accd21309c
Hanz <40712686+HanzCEO@users.noreply.github.com>
2022-02-15 08:21:41 +0700

    Bumped version to v1.0.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
 	name="DisMail",
-	version="1.0.0",
+	version="1.0.1",
 	description="CLI to help you read your disposable emails straight from your console",
 	long_description=README,
 	long_description_content_type="text/markdown",
